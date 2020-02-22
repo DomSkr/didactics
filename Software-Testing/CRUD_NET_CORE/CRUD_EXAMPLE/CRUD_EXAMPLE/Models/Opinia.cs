f591df3ed8088d613135934a753072797115af9d
Kamil Lepek <kamil.lepek94@gmail.com>
2020-02-22 23:22:10 +0100

    CRUD example for software testing
    Signed-off-by: Kamil Lepek <kamil.lepek94@gmail.com>

---
+++ Software-Testing/CRUD_NET_CORE/CRUD_EXAMPLE/CRUD_EXAMPLE/Models/Opinia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_EXAMPLE.Models
+{
+    public class Opinia
+    {
+        // ocena 1-5
+        // string komentarz - może rzucać 500 jeśl polskie znaki?
+        // string nickname
+        // zrobić local db + CRUD
+        // Zostawić jakieś podstronki bez sensu (ale niech main wygląda ok)
+        // Zbugować responsywne css dla mniejszych rozdziałek
+    }
+}
